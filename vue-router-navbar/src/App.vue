015d96cc8286c6a5f68a9a9a99a336d6a0b361a3
KovacsBence19 <71010714+KovacsBence19@users.noreply.github.com>
2025-12-03 15:27:44 +0100

    Add files via upload

---
+++ vue-router-navbar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- Navigációs sáv -->
+    <Navbar />
+    <!-- Itt jelennek meg az útvonalak tartalmai -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+}
+</script>
